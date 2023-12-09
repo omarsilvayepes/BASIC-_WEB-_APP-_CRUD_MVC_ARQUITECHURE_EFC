b96de3137efae480a9f46d6fa926dd09d02d14fc
omarsilvayepes <omar.yepes@hotmail.com>
2023-12-09 16:46:29 -0500

    Delete and update Category service And toastr messages

--- WebAppMVCArquitecture/Controllers/CategoryController.cs
+++ WebAppMVCArquitecture/Controllers/CategoryController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using WebAppMVCArquitecture.Data;
 using WebAppMVCArquitecture.Models;
 
@@ -14,29 +13,73 @@ namespace WebAppMVCArquitecture.Controllers
         }
         public IActionResult Index()
         {
-            List<Category> listCategories=_dbContext.Categories.ToList();
+            List<Category> listCategories = _dbContext.Categories.ToList();
             return View(listCategories);
         }
 
-        public IActionResult Create()
+        public IActionResult Create() // for show the view to the user 
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Create(Category category)
+        public IActionResult Create(Category category) // for save changes in DB
         {
             if (category.Name == category.DisplayOrder.ToString())
             {
-                ModelState.AddModelError("name","The Name Can Not exactly match with the Display Order Field");
+                ModelState.AddModelError("name", "The Name Can Not exactly match with the Display Order Field");
             }
             if (ModelState.IsValid)
             {
                 _dbContext.Categories.Add(category);
                 _dbContext.SaveChanges();
+                TempData["Success"] = "Create Succesfully";
                 return RedirectToAction("Index", "Category");
             }
             return View();
         }
+
+        public IActionResult Edit(int? id) // for show the view to the user
+        {
+            if (id == null || id == 0) return NotFound();
+
+            Category? categoryDb = _dbContext.Categories.FirstOrDefault(c=> c.Id == id);
+            if (categoryDb == null) return NotFound();
+
+            return View(categoryDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category) // for save changes in DB
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(category);
+                _dbContext.SaveChanges();
+                TempData["Success"] = "Updated Succesfully";
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id) // for show the view to the user
+        {
+            if (id == null || id == 0) return NotFound();
+
+            Category? categoryDb = _dbContext.Categories.FirstOrDefault(c => c.Id == id);
+            if (categoryDb == null) return NotFound();
+
+            return View(categoryDb);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Category category) // for save changes in DB
+        {
+            _dbContext.Categories.Remove(category);
+            _dbContext.SaveChanges();
+            TempData["Success"] = "Deleted Succesfully";
+            return RedirectToAction("Index", "Category");
+        }
+
     }
 }
